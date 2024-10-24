fafface98f496a5cc4bd370e098c4294f3f4dc24
Robert Farlow <robfarlow@gmail.com>
2024-10-24 13:26:19 -0700

    refactor(plato_client): update methods to async and add PlatoConfig class
    chore: update dependencies and add pydantic to poetry lock and pyproject.toml

--- python/plato/plato_client.py
+++ python/plato/plato_client.py
@@ -1,25 +1,37 @@
 
+from typing import Optional
+from pydantic import BaseModel
+
 BASE_URL = "https://api.plato.so"
 
 class PlatoSession:
   def __init__(self, session_id: str):
     self.session_id = session_id
 
-  def act(self, action: str, **kwargs):
+  async def start(self):
+    # get a browser connection
+    # then connect to server with socketio connection
     pass
 
-  def extract(self, url: str, **kwargs):
+  async def act(self, action: str, **kwargs):
     pass
 
-  def monitor(self, url: str, **kwargs):
+  async def extract(self, url: str, **kwargs):
     pass
 
-  def job(self, job_id: str, **kwargs):
+  async def monitor(self, url: str, **kwargs):
+    pass
+
+  async def job(self, job_id: str, **kwargs):
     pass
 
 
+class PlatoConfig(BaseModel):
+  api_key: str
+  base_url: Optional[str] = BASE_URL
 
 class PlatoClient:
   @staticmethod
-  def start_session() -> PlatoSession:
-    pass
+  async def start_session(config: PlatoConfig) -> PlatoSession:
+    return await PlatoSession(session_id="123").start()
+
