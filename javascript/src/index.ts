870aab5dc3b895202f6586158fcf1cc4aabdba37
sohan sarabu <sohan@plato.so>
2025-10-29 17:36:22 +0000

    remove bloat

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -14,22 +14,3 @@
 export { Plato, PlatoEnvironment, PlatoTask } from './plato/client';
 export { PlatoClientError } from './plato/exceptions';
 export { Config } from './plato/config';
-
-// Native Bindings (Node.js only - full sandbox SDK)
-// Import from 'plato-sdk/native'
-export { PlatoSandboxClient } from './plato/native-bindings';
-export type {
-  SimConfigDataset,
-  SimConfigCompute,
-  SimConfigMetadata,
-  Sandbox,
-  CreateSnapshotRequest,
-  CreateSnapshotResponse,
-  StartWorkerRequest,
-  StartWorkerResponse,
-  SimulatorListItem,
-  GiteaCredentials,
-  GiteaRepository,
-  ProxyTunnelInfo,
-  SSHInfo,
-} from './plato/native-bindings';
