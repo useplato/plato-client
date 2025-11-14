8b884cbae10b2c035df87078cd112817641028fb
Sohan Sarabu <sohan.sarabu@gmail.com>
2025-11-14 10:06:54 -0800

    remove more nonsense

--- sdk/typescript-sdk-fern-backup/src/plato/_generated/api/resources/publicBuild/client/requests/index.ts
+++
@@ -1,4 +0,0 @@
-export type { CreateVmRequest } from "./CreateVmRequest.js";
-export type { SetupRootPasswordRequest } from "./SetupRootPasswordRequest.js";
-export type { SetupSandboxRequest } from "./SetupSandboxRequest.js";
-export type { VmManagementRequest } from "./VmManagementRequest.js";
--- sdk/typescript-sdk-fern-backup/src/plato/_generated/index.ts
+++
@@ -1,5 +0,0 @@
-export * as Api from "./api/index.js";
-export type { BaseClientOptions, BaseRequestOptions } from "./BaseClient.js";
-export { ApiClient } from "./Client.js";
-export { ApiError, ApiTimeoutError } from "./errors/index.js";
-export * from "./exports.js";
